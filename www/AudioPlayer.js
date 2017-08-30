80056e6a746b143877c6172550c5f2463dac0f98
phi <phi1618jp@gmail.com>
2017-08-31 03:56:10 +0900

    setup audio player

--- www/AudioPlayer.js
+++ www/AudioPlayer.js
@@ -125,34 +125,6 @@ Media.prototype.getCurrentPosition = function(success, fail) {
 };
 
 /**
- * Start recording audio file.
- */
-Media.prototype.startRecord = function() {
-    exec(null, this.errorCallback, "Media", "startRecordingAudio", [this.id, this.src]);
-};
-
-/**
- * Stop recording audio file.
- */
-Media.prototype.stopRecord = function() {
-    exec(null, this.errorCallback, "Media", "stopRecordingAudio", [this.id]);
-};
-
-/**
- * Pause recording audio file.
- */
-Media.prototype.pauseRecord = function() {
-    exec(null, this.errorCallback, "Media", "pauseRecordingAudio", [this.id]);
-};
-
-/**
-* Resume recording audio file.
-*/
-Media.prototype.resumeRecord = function() {
-    exec(null, this.errorCallback, "Media", "resumeRecordingAudio", [this.id]);
-};
-
-/**
  * Release the resources.
  */
 Media.prototype.release = function() {
