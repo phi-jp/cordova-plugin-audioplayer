5aaf61aa2b67220d20520b7823970d6f16ca89c3
phi <phi1618jp@gmail.com>
2017-08-31 08:30:51 +0900

    remove files

--- www/AudioPlayerError.js
+++ www/AudioPlayerError.js
@@ -23,33 +23,33 @@
  * This class contains information about any Media errors.
 */
 /*
- According to :: http://dev.w3.org/html5/spec-author-view/video.html#mediaerror
+ According to :: http://dev.w3.org/html5/spec-author-view/video.html#AudioPlayerError
  We should never be creating these objects, we should just implement the interface
  which has 1 property for an instance, 'code'
 
  instead of doing :
-    errorCallbackFunction( new MediaError(3,'msg') );
+    errorCallbackFunction( new AudioPlayerError(3,'msg') );
 we should simply use a literal :
     errorCallbackFunction( {'code':3} );
  */
 
- var _MediaError = window.MediaError;
+ var _AudioPlayerError = window.AudioPlayerError;
 
 
-if(!_MediaError) {
-    window.MediaError = _MediaError = function(code, msg) {
+if(!_AudioPlayerError) {
+    window.AudioPlayerError = _AudioPlayerError = function(code, msg) {
         this.code = (typeof code != 'undefined') ? code : null;
         this.message = msg || ""; // message is NON-standard! do not use!
     };
 }
 
-_MediaError.MEDIA_ERR_NONE_ACTIVE    = _MediaError.MEDIA_ERR_NONE_ACTIVE    || 0;
-_MediaError.MEDIA_ERR_ABORTED        = _MediaError.MEDIA_ERR_ABORTED        || 1;
-_MediaError.MEDIA_ERR_NETWORK        = _MediaError.MEDIA_ERR_NETWORK        || 2;
-_MediaError.MEDIA_ERR_DECODE         = _MediaError.MEDIA_ERR_DECODE         || 3;
-_MediaError.MEDIA_ERR_NONE_SUPPORTED = _MediaError.MEDIA_ERR_NONE_SUPPORTED || 4;
-// TODO: MediaError.MEDIA_ERR_NONE_SUPPORTED is legacy, the W3 spec now defines it as below.
+_AudioPlayerError.MEDIA_ERR_NONE_ACTIVE    = _AudioPlayerError.MEDIA_ERR_NONE_ACTIVE    || 0;
+_AudioPlayerError.MEDIA_ERR_ABORTED        = _AudioPlayerError.MEDIA_ERR_ABORTED        || 1;
+_AudioPlayerError.MEDIA_ERR_NETWORK        = _AudioPlayerError.MEDIA_ERR_NETWORK        || 2;
+_AudioPlayerError.MEDIA_ERR_DECODE         = _AudioPlayerError.MEDIA_ERR_DECODE         || 3;
+_AudioPlayerError.MEDIA_ERR_NONE_SUPPORTED = _AudioPlayerError.MEDIA_ERR_NONE_SUPPORTED || 4;
+// TODO: AudioPlayerError.MEDIA_ERR_NONE_SUPPORTED is legacy, the W3 spec now defines it as below.
 // as defined by http://dev.w3.org/html5/spec-author-view/video.html#error-codes
-_MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED = _MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED || 4;
+_AudioPlayerError.MEDIA_ERR_SRC_NOT_SUPPORTED = _AudioPlayerError.MEDIA_ERR_SRC_NOT_SUPPORTED || 4;
 
-module.exports = _MediaError;
+module.exports = _AudioPlayerError;
